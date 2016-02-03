a1090475f78114e218dc28a125cb87efbd31e483
chandni-s <c.sehgal12@gmail.com>
2016-02-02 20:45:27 -0500

    fixed import bug

--- A1/ClientSide/src/clientSide/A1Sender.java
+++ A1/ClientSide/src/clientSide/A1Sender.java
@@ -12,6 +12,7 @@ import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileReader;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 
 /**
