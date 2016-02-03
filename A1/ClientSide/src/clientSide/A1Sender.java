2bbd743b85ee7a76036fbeec25b7f6eacd424f75
Srihitha <srihitha.maryada@gmail.com>
2016-02-03 18:52:50 -0500

    Part 1 works with one small bug

--- A1/ClientSide/src/clientSide/A1Sender.java
+++ A1/ClientSide/src/clientSide/A1Sender.java
@@ -6,16 +6,10 @@ package clientSide;
 import java.io.IOException;
 import java.net.Socket;
 import java.net.SocketAddress;
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.InputStream;
 import java.io.OutputStream;
 
 /**
@@ -68,72 +62,21 @@ public class A1Sender {
                     }
                 }
             }
-            writeToFile(content);
-            //System.out.println(content);
             
-            try {
-                fr.close();
-                br.close();
-                clientSock.close();
-            } catch (Exception ex) {
-                System.out.println(ex.getMessage());
-            }
+            writeToServer(content);
+            
+            fr.close();
+            br.close();
+            clientSock.close();
             
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
     
-    public static void writeToFile(String content){
-    	File file = new File ("../ServerSide/StreamReceived.txt");
-    	try {
-			FileWriter fw = new FileWriter(file.getAbsoluteFile());
-			BufferedWriter bw = new BufferedWriter(fw);
-			bw.write(content);
-			bw.close();
-			
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-    }
-    
-    public static void connect(){
-        
-        try{
-            //Connecting to server
-            System.out.println("Client socket connecting on: " + server
-                               + " port: " + port);
-            
-            clientSock = new Socket(server, port);
-            System.out.println("Client connected to Server: "
-                               +clientSock.isConnected());
-            
-        }
-        
-        catch (IOException e) {
-            e.printStackTrace();
-            try {
-                clientSock.close();
-            } catch (IOException ex) {
-                ex.getMessage();
-            }
-            System.exit(1);
-        }
-        
-        
-    }
     
     public static void sendInitialGreeting() {
-        
-        
-        OutputStream outToServer = null;
-        DataOutputStream toServer = null;
-        
-        InputStream inFromServer = null;
-        DataInputStream inServer = null;
-        
-        
+    	
         try {
             
             //Connect to server
@@ -142,32 +85,46 @@ public class A1Sender {
             SocketAddress ip_address = clientSock.getLocalSocketAddress();
             System.out.println("<IP Address>:" + ip_address);
             
-            //Send initial greeting to server
-            outToServer = clientSock.getOutputStream();
-            toServer = new DataOutputStream(outToServer);
-            toServer.writeUTF(ip_address + ":<" + port + ">");
-            toServer.writeUTF("\n");  // delimiter
+            String ip_adr = ip_address + ":<" + port + ">";
+            writeToServer(ip_adr);
             
-            //Read from server
+            /*//Read from server
             inFromServer = clientSock.getInputStream();
             inServer = new DataInputStream(new BufferedInputStream (inFromServer));
             inServer = new DataInputStream(inFromServer);
             //String serverMsg = new String (inServer.read());
-            System.out.println("FromServer:" + inServer.read()); 
+            System.out.println("FromServer:" + inServer.read());*/ 
             
-            toServer.close();
-            inServer.close();
             
         } catch (IOException e) {
             e.printStackTrace();
-            try {
-                toServer.close();
-                inServer.close();
-                clientSock.close();
-            } catch (IOException ex) {
-                ex.getMessage();
-            }
             System.exit(1);
         }
     }
+    
+	 public static void connect(){
+	        
+	        try{
+	            //Connecting to server
+	            System.out.println("Client socket connecting on: " + server
+	                               + " port: " + port);
+	            
+	            clientSock = new Socket(server, port);
+	            System.out.println("Client connected to Server: "
+	                               +clientSock.isConnected());
+	        } 
+	        catch (IOException e) {
+	            e.printStackTrace();
+	            System.exit(1);
+	        }	        	        
+	  }
+	 
+	 
+	 public static void writeToServer(String content) throws IOException{
+		 
+		 OutputStream outToServer = clientSock.getOutputStream();
+         DataOutputStream toServer = new DataOutputStream(outToServer);
+         toServer.writeUTF(content);
+		 
+	 }
 }
