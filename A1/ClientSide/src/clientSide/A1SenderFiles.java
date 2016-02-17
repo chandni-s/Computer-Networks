59f5aecf1cc116148998b36e824afc84d1b5f834
chandni-s <c.sehgal12@gmail.com>
2016-02-16 23:00:48 -0500

    Created conn & structure for transfering files+content

--- A1/ClientSide/src/clientSide/A1SenderFiles.java
+++ A1/ClientSide/src/clientSide/A1SenderFiles.java
@@ -7,11 +7,83 @@ import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
 
 /**
  * @author 
  *
  */
 public class A1SenderFiles {
+	
+	
+	private final static String serverFile = "localhost";
+	private final static int port = 7777;
+	static Socket clientFileSock;
+	
+	
+	public static void main (String[] args) {
+		sendTheseFiles(3);
+	}
 
+	private static void sendTheseFiles(int k) {
+		
+		connect();
+		
+		if (k < 1) {
+			System.out.println("No files to send, please specify > 0");
+			
+		}
+		
+		
+		
+		try {
+			writeToServer("Got to SenderFiles on client side");
+			
+			//TODO
+			// send the file name and length of file to server
+			// set a delimiter and send it to server to mark end of file
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public static void connect() {
+
+		try {
+			// Connecting to server
+			System.out.println("ClientFiles socket connecting on: " + serverFile
+					+ " port: " + port);
+
+			clientFileSock = new Socket(serverFile, port);
+			System.out.println("ClientFiles connected to ServerFiles: "
+					+ clientFileSock.isConnected());
+			
+			
+		} catch (IOException e) {
+			
+			System.out.println("ClientFiles socket failed to connect to serverFiles " + e.getMessage());
+			e.printStackTrace();
+
+			try {
+				clientFileSock.close();
+			} catch (IOException e1) {
+				System.out.println("ClientFiles socket failed at closing connection: " + e1.getMessage());
+				e1.printStackTrace();
+			}
+			System.exit(1);
+		}
+	}
+	
+	public static void writeToServer(String content) throws IOException {
+
+		OutputStream outToServer = clientFileSock.getOutputStream();
+		DataOutputStream toServer = new DataOutputStream(outToServer);
+		toServer.writeUTF(content);
+		toServer.flush();
+	}
 }
--- A1/ServerSide/src/mySocket/A1ReceiverFiles.java
+++ A1/ServerSide/src/mySocket/A1ReceiverFiles.java
@@ -1,5 +1,87 @@
 package mySocket;
 
-public class A1ReceiverFiles {
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.Socket;
 
+public class A1ReceiverFiles extends Thread {
+
+	private Socket socket;
+
+	// call this in A1Listener
+	public A1ReceiverFiles(Socket socket) {
+		// TODO Auto-generated constructor stub
+		this.socket = socket;
+	}
+
+	public void run() {
+
+		readWriteData();
+
+		try {
+			socket.close();
+		} catch (Exception ex) {
+			System.out.println("ReceiverFiles error in closing server: "
+					+ ex.getMessage());
+		}
+	}
+
+	private void readWriteData() {
+
+		// set the connection
+		DataInputStream dataInFromClient = null;
+		DataOutputStream dataOutToClient = null;
+		String msgFromServer = null;
+
+		// receive and send data to and from clientFiles
+		try {
+			dataInFromClient = new DataInputStream(new BufferedInputStream(
+					socket.getInputStream()));
+
+			dataOutToClient = new DataOutputStream(new BufferedOutputStream(
+					socket.getOutputStream()));
+
+			msgFromServer = dataInFromClient.readUTF();
+
+			System.out.println("Message from client: " + msgFromServer);
+
+			dataOutToClient.writeUTF("Server Listening to clientFiles");
+			
+			readWriteDataToFile();
+			
+		}
+
+		// if any error in connecting to clientFiles - close socket and exit
+		catch (Exception e) {
+
+			System.out.println("Error in setting up ServerFiles: "
+					+ e.getMessage());
+
+			try {
+				socket.close();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			System.exit(1);
+
+			e.printStackTrace();
+		}
+	}
+
+	private void readWriteDataToFile() {
+		// TODO Auto-generated method stub
+		
+		// file name and file size length from
+		// client - create test dir if it doens't exist 
+		// create empty files of same name
+		// write content of files received from client to files just created
+		// once the content is delimiter and size of this new file that is
+		// being written matches the file size sent by client - end the file
+		// and start writing new one if needed.
+	}
 }
