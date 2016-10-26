689484d1a4cf07b7f924ba68fc775d91944a6ece
chandni-s <c.sehgal12@gmail.com>
2016-10-25 23:23:23 -0400

    rebasing

--- A3/DVRouter/src/DVRouter/DVRouter.java
+++ A3/DVRouter/src/DVRouter/DVRouter.java
@@ -1,15 +1,12 @@
-package DVRouter;
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
-<<<<<<< HEAD
 import java.util.List;
 
 /**
@@ -18,16 +15,14 @@ import java.util.List;
  *
  */
 public class DVRouter {
-	
+
 	protected static String seqNum = null;
 	protected static String routerID = null;
+	protected static int routerToDv = 0;
 
-	// Routing Table = Destination: <Next Hop, Cost>
 	private static HashMap<String, RoutingTableType> dvrouter = new HashMap<String, RoutingTableType>();
-	
-	//DVUpdate
 	private static HashMap<String, Integer> dvUpdate = new HashMap<String, Integer>();
-	
+	static List<String> update = new ArrayList<String>();
 
 	protected class RoutingTableType {
 		String destination;
@@ -36,53 +31,23 @@ public class DVRouter {
 
 		public RoutingTableType(String destination, String nextHop, int totalCost) {
 			this.destination = destination;
-=======
-
-/**
- * @Name Chandni Sehgal StudentNo: 998973375
- * @Name Srihitha Maryada StudentNo: Infinity :)
- *
- */
-public class DVRouter {
-
-	// Destination <NextHop:Cost>
-	private static HashMap<String, RoutingTable> dvrouter = new HashMap<String, RoutingTable>();
-
-	protected class RoutingTable {
-		String destination;
-		String nextHop;
-		int totalCost;
-
-<<<<<<< HEAD
-		public RoutingTable(String nextHop, int totalCost) {
->>>>>>> 9081eaa... Added A3
-=======
-		public RoutingTable(String destination, String nextHop, int totalCost) {
-			this.destination = destination;
->>>>>>> 25c2987... changed routing table object
 			this.nextHop = nextHop;
 			this.totalCost = totalCost;
 		}
 	}
 
-<<<<<<< HEAD
 	public static List<String> readRoutingTable(String filename) {
-=======
-	public static void readRoutingTable(String filename) {
->>>>>>> 9081eaa... Added A3
+
+		/*
+		 * Read each update and add it into a list of string Return a list of
+		 * updates
+		 */
 
 		File file = null;
 		FileReader readFile = null;
 		BufferedReader bufRead = null;
-		// ArrayList<String> content = new ArrayList<String>();
-		String[] hopCost = null;
 
-		int sequenceNum = 0;
-		String routerID = null;
-<<<<<<< HEAD
 		List<String> updateList = new ArrayList<String>();
-=======
->>>>>>> 9081eaa... Added A3
 
 		try {
 			file = new File(filename);
@@ -90,77 +55,21 @@ public class DVRouter {
 			bufRead = new BufferedReader(readFile);
 
 			String line;
-<<<<<<< HEAD
 			String update = "";
-			
 
-			int count = 0;
 			while ((line = bufRead.readLine()) != null) {
-				if (line.length() != 0){
+
+				if (line.length() != 0) {
 					update += line + "\n";
-					//System.out.println(update);
-				}
-				else{
-					//System.out.println(update);
+
+				} else {
 					updateList.add(update);
 					update = "";
 				}
-				
-
-				/*// Get the sequence number
-=======
-
-			int count = 0;
-			while ((line = bufRead.readLine()) != null) {
-
-				// Get the sequence number
->>>>>>> 9081eaa... Added A3
-				if (count == 0) {
-					sequenceNum = Integer.parseInt(line);
-					count += 1;
-				}
-
-				// get routerID
-				else if (count == 1) {
-					routerID = line;
-					count += 1;
-				}
-
-				// get Hops and Cost
-				else {
-					hopCost = line.trim().split(";");
-					count += 1;
-					if (hopCost != null && hopCost.length > 1) {
-<<<<<<< HEAD
-						//System.out.println(hopCost[0]);
-
-						// Create object
-						//RoutingTable rt = updateRoutingTable(routerID, hopCost);
-						// put routerID and Routing object into hashMap
-						
-					}
-					if (line.length() == 0) {
-						count = 0;
-						//System.out.println("Setting null");
-						dvUpdate = null;
-					}
-				}*/
-				
 			}
+
+			// add the last update to string array
 			updateList.add(update);
-=======
-
-						// Create object
-						RoutingTable rt = updateRoutingTable(routerID, hopCost);
-						// put routerID and Routing object into hashMap
-						
-					}
-					if (line.length() == 0) {
-						count = 0;
-					}
-				}
-			}
->>>>>>> 9081eaa... Added A3
 
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -177,157 +86,198 @@ public class DVRouter {
 			}
 
 		}
-<<<<<<< HEAD
-		//System.out.print(updateList);
+
 		return updateList;
+	}
+
+	public static void dvUpdateTable(String update) {
+		/*
+		 * Parses DVUpdate and puts all the data into dvUpdate Hashmap. Calls
+		 * helper method to update router Dv's routing table.
+		 */
+
+		// Get the sequence number and routerID from
+		String[] attributes = update.split("\n");
+		seqNum = attributes[0];
+		routerID = attributes[1];
+
+		// get all the destinations and its cost and put them into dvUpdate
+		// HashMap
+		for (int i = 2; i < attributes.length; i++) {
+			String[] dvKeys = attributes[i].split(";");
+			int cost = Integer.parseInt(dvKeys[1].trim());
+			String dvDest = dvKeys[0];
+			dvUpdate.put(dvDest, cost);
+			// Gets cost from current router to DV and updates global variable
+			if (dvDest.equals("DV")) {
+				routerToDv = cost;
+			}
+		}
+		// Call helper method that updates dv router's routing table
+		DVUpdateHopCost();
+	}
+
+	public static void DVUpdateHopCost() {
+		/*
+		 * Updates DV router's routing table with the given DVUpdat.
+		 */
+
+		for (String key : dvUpdate.keySet()) {
+
+			int costUpdate;
+			int newCost = 0;
+			String dest;
+			costUpdate = dvUpdate.get(key);
+
+			// if DVUpdate has "DV" for destination then update
+			// destination to routerID
+			if (key.equals("DV")) {
+				dest = routerID;
+				newCost = costUpdate;
+			}
+
+			else {
+				dest = key;
+			}
+
+			// if RoutingTableType already in DV's routing table
+			if (dvrouter.containsKey(dest)) {
+				RoutingTableType rt = dvrouter.get(dest);
+				int dvCost = rt.totalCost;
+
+				if (costUpdate < dvCost) {
+					rt.nextHop = routerID;
+
+					// update totalCost
+					if (newCost == 0) {
+						// System.out.println(routerToDv);
+						newCost = routerToDv + costUpdate;
+						rt.totalCost = newCost;
+					}
+					rt.totalCost = newCost;
+				}
+			}
+
+			else {
+				// Add new RoutingTableType to DV routing table
+				if (newCost == 0) {
+					newCost = routerToDv + costUpdate;
+				}
+				updateRoutingTable(dest, newCost);
+
+			}
+		}
+		// Call helper method to write contents to file
+		writeOutFile();
 	}
 
 	public static RoutingTableType updateRoutingTable(String dest, int cost) {
 
+		/*
+		 * Create and return an object for each update and put it into a routing
+		 * HashMap
+		 */
+
 		RoutingTableType rt = null;
 		DVRouter dv = new DVRouter();
 
-		// Create object
 		rt = dv.new RoutingTableType(dest, routerID, cost);
-		//System.out.println(rt.nextHop + " GOOOT " + rt.totalCost);
-		
-		
+
 		dvrouter.put(rt.destination, rt);
-		//System.out.println("Keys: " + dvrouter.keySet() + "Values: ");
-		
+
 		return rt;
 	}
-	
-	public static void dvUpdateTable(String update){
-		
-		String [] attributes = update.split("\n");
-		seqNum = attributes[0];
-		routerID = attributes[1];
-		//System.out.println(seqNum + ":" + routerID);
-		
-		//dvUpdate.put(routerID, 0);
-		
-		for (int i = 2; i < attributes.length; i++){
-			String [] dvKeys = attributes[i].split(";");
-			int cost = Integer.parseInt(dvKeys[1].trim());
-			dvUpdate.put(dvKeys[0], cost);
-		}
-		/*for (String key: dvUpdate.keySet()) {
-		//RoutingTable obj = dvrouter.get(key);
-		System.out.println("Key:" + key + " Values:" + dvUpdate.get(key));
-		}*/
-		}
-	
-	public static void routingTable(){
-		
-		String dest;
-		int costUpdate;
-		int toDv = 0;
-		int newCost = 0;
-		
-		for (String key: dvUpdate.keySet()){
-			dest = key;
-			costUpdate = dvUpdate.get(key);
-			
-			System.out.println("key " + key);
-			
-			if (key.equals("DV")){
-				dest = routerID;
-				toDv = dvUpdate.get(key);
-				newCost = toDv;
-				//System.out.println(newCost);	
+
+	public static void writeOutFile() {
+		/*
+		 * Writes the processed updates to OutUpdates.txt
+		 */
+
+		File msgFile = null;
+		FileWriter msgout = null;
+		BufferedWriter bufMsg = null;
+
+		try {
+
+			msgFile = new File("./OutUpdates.txt");
+
+			msgout = new FileWriter(msgFile, true);
+			bufMsg = new BufferedWriter(msgout);
+
+			// write the sequence number
+			bufMsg.write("");
+			bufMsg.write(seqNum + "\n");
+			bufMsg.flush();
+
+			// write the hops and its corresponding update cost
+			for (String key : dvrouter.keySet()) {
+				RoutingTableType obj = dvrouter.get(key);
+
+				bufMsg.write("");
+				bufMsg.write(key + "; " + obj.nextHop + "; " + obj.totalCost + "\n");
+				bufMsg.flush();
 			}
-			
-			if (dvrouter.containsKey(dest)){
-				//System.out.println("Has Key: "+ dest);
-				RoutingTableType rt = dvrouter.get(dest);
-				int dvCost = rt.totalCost;
-				System.out.println("DVCost " + dvCost);
-				System.out.println("Update " + costUpdate);
-				if (costUpdate < dvCost){
-					//System.out.println("Update " + costUpdate);
-					rt.nextHop = routerID;
-					
-					if (newCost == 0){
-						newCost = toDv + costUpdate;
-						rt.totalCost = newCost;
-					}
-					rt.totalCost = newCost;
-					
-					//System.out.println(costUpdate);
-					
+
+			// add new line after each sequence update
+			bufMsg.write("");
+			bufMsg.write("\n");
+			bufMsg.flush();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+
+		} finally {
+			if (msgFile != null) {
+				try {
+					bufMsg.close();
+					msgout.close();
+				} catch (IOException e) {
+					e.printStackTrace();
 				}
 			}
-			
-			else{
-				//Add new RoutingTableType
-				if (newCost == 0){
-					newCost = toDv + costUpdate;
-				}
-				updateRoutingTable(dest, newCost);
-				
-			}
-			newCost = 0;
-			toDv = 0;
-			costUpdate = 0;
 		}
-		for (String key: dvrouter.keySet()) {
-			RoutingTableType obj = dvrouter.get(key);
-			System.out.println("Key:" + key + " Values:" + obj.nextHop + " " + obj.totalCost);
+	}
+
+	public static void clearLogFile(String fileName) {
+
+		/*
+		 * Clears the text file if exists, creates a file if it doesn't exist.
+		 * Used to clear OutUpdates.txt
+		 */
+
+		File file = new File(fileName);
+
+		if (file.exists()) {
+			try {
+				FileWriter fw = new FileWriter(file.getAbsolutePath());
+				PrintWriter pw = new PrintWriter(fw);
+				pw.write("");
+				pw.flush();
+				pw.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		else {
+			try {
+				file.createNewFile();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 
 	public static void main(String[] args) {
-		
+
+		clearLogFile("OutUpdates.txt");
+
 		List<String> updateList = new ArrayList<String>();
 		updateList = readRoutingTable("InUpdates.txt");
-		//dvUpdateTable(updateList.get(0));
-		//routingTable();
-		
-		
-		//System.out.println(updateList);
-		for (int i=0; i < updateList.size(); i++){
+
+		for (int i = 0; i < updateList.size(); i++) {
+			dvUpdate.clear();
 			dvUpdateTable(updateList.get(i));
-			routingTable();
 		}
-		
-		
-		
-		/*for (String key: dvrouter.keySet()) {
-			RoutingTable obj = dvrouter.get(key);
-			System.out.println("Key:" + key + " Values:" + obj.nextHop + " " + obj.totalCost);
-		}*/
-=======
-	}
-
-	public static RoutingTable updateRoutingTable(String routerID, String[] hopCost) {
-
-		RoutingTable rt = null;
-		DVRouter dv = new DVRouter();
-
-		// Create object
-		rt = dv.new RoutingTable(routerID, hopCost[0],Integer.parseInt(hopCost[1].trim()));
-		//System.out.println(rt.nextHop + " GOOOT " + rt.totalCost);
-		
-		
-		dvrouter.put(rt.destination, rt);
-		//System.out.println("Keys: " + dvrouter.keySet() + "Values: ");
-		
-		
-
-		return rt;
-	}
-
-	public static void main(String[] args) {
-
-		readRoutingTable("InUpdates.txt");
-<<<<<<< HEAD
->>>>>>> 9081eaa... Added A3
-=======
-		for (int i=0; i<dvrouter.size(); i++) {
-
-			System.out.println("Values: " + dvrouter.get(i));
-		}
->>>>>>> 25c2987... changed routing table object
 	}
 }
